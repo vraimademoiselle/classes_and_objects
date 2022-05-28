f293e1897e7ff4a2deedbac57f33fcb447480a1a
Fatma Osman <99663006+vraimademoiselle@users.noreply.github.com>
2022-05-28 22:38:00 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+class Student:
+    # [assignment] Skeleton class. Add your code here
+        pass
+# [assignment] Skeleton class. Add your code here
+from msilib.schema import SelfReg
+
+def _init_(self, Name, age, tracks, score):
+        self.Name = Name
+        self.age = age
+        self.tracks = tracks
+        self.score = score
+    #create the first object
+    #object of student
+class Student:
+    Name = "Bob"
+    age = 34
+    tracks = "UI/UX"
+    score = 10.11
+    print('class student: ')
+    print('Name is a', Name)
+    print('age is a', age)
+    print('track is a', tracks)
+    print('score is a', score)
+
+print("\nAccessing class variable using class name")
+print(SelfReg.name)
